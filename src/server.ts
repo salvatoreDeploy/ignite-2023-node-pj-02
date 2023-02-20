679e5b2d4eeda3579cf10d7c6b6b38e0ece36530
salvatoreDeploy <liderhenrique@gmail.com>
2023-02-20 14:42:06 -0300

    configuração DB com Knex e Sqlite3

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,13 @@
 import fastify from "fastify";
+import { knex } from "./database";
 
 const app = fastify();
+
+app.get("/testedb", async () => {
+  const table = await knex("sqlite_schema").select("*");
+
+  return table;
+});
 
 app
   .listen({
